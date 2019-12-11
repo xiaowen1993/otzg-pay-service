a92802855c0189b8b23e82c50145c7a04588e57b
Administrator <375214167>
2019-12-11 18:03:33 +0800

    v1.02

---
+++ src/main/06-alipay/com/bcb/alipay/util/face/api/AlipayCallBack.java
@@ -0,0 +1,12 @@
+package com.bcb.alipay.util.face.api;
+
+
+import com.alipay.api.AlipayResponse;
+
+/**
+ * Created by bruce on 2017/12/25.
+ */
+
+public interface AlipayCallBack {
+    <T extends AlipayResponse> T onResponse(T response);
+}
---
+++ src/main/06-alipay/com/bcb/alipay/util/face/api/AlipayHashMap.java
@@ -0,0 +1,19 @@
+package com.bcb.alipay.util.face.api;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by bruce on 2017/12/25.
+ */
+public class AlipayHashMap extends HashMap {
+
+    public AlipayHashMap() {
+
+    }
+
+    public AlipayHashMap(Map<String, String> textParams) {
+        this.putAll(textParams);
+    }
+}
